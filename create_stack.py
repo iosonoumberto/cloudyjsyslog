5b297b495cf9701ac85c66095d9e29e3e8f508e9
iosonoumberto <umberto.manferdini@gmail.com>
2020-10-29 06:54:49 -0700

    device syslog configuration. verified

--- create_stack.py
+++ create_stack.py
@@ -5,6 +5,10 @@ import yaml
 from jinja2 import Template
 import supportlib
 
+from jnpr.junos import Device
+from jnpr.junos.exception import *
+from jnpr.junos.utils.config import Config
+
 sd=os.getcwd()
 
 fi=open('stack_conf.yaml','r')
@@ -38,3 +42,37 @@ supportlib.create_file('fluent.conf',sconf,"")
 os.chdir(sconf['working_path'])
 subprocess.run(["docker-compose", "up", "-d"])
 os.chdir(sd)
+
+for d in sconf['devices']:
+    dev=Device(host=d['ip'], user=d['user'], password=d['pass'])
+    try:
+        dev.open()
+    except ConnectError as err:
+        print ("ERROR Cannot connect to device: {0}".format(err))
+        exit()
+    except Exception as err:
+        print ("ERROR while connecting to device - " + str(err))
+        exit()
+    try:
+        cfg=Config(dev, mode='private')
+    except Exception as err:
+        print("[ERROR] Cannot open configuration - " + str(err))
+        dev.close()
+        sys.exit()
+    try:
+        cfg.lock()
+    except Exception as err:
+        print("[ERROR] Cannot lock configuration - " + str(err))
+        dev.close()
+        sys.exit()
+    logvars= { 'log_server' : sconf['log_server'], 'src_log' : d['src']}
+    cfg.load(template_path=str('jinja_templates/junos.conf.j2'), template_vars=logvars, format='set')
+    try:
+        cfg.commit(comment="Device sending log to cloudyjsyslog")
+    except Exception as err:
+        print("[ERROR] Cannot commit configuration - " + str(err))
+        print("[ERROR] Rollback and exit")
+        cfg.rollback()
+        dev.close()
+        sys.exit()
+    dev.close()
